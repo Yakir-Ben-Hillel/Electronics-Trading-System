47458365aed84ef76864ecf283bb7ba6936de386
Maor <50876799+MBM9@users.noreply.github.com>
2019-11-13 13:35:25 +0200

    Update product.h

--- include/product.h
+++ include/product.h
@@ -8,6 +8,16 @@ class Product
     public:
 		enum eCategory {Children,Electricity,Office,Clothing};
 
+		bool setName(const char* name);
+        bool setPrice(float price);
+        bool setSerialNumber(unsigned int serial);
+		bool setCategory(eCategory category);
+
+		char* getName() const;
+		float getPrice() const;
+		eCategory getCategory() const;
+		unsigned int getSerialNumber() const;
+
     private:
 		eCategory p_category;
 		char* p_name;
