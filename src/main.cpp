09eff68bcda78c873d1038afee714260d213d3c0
Maor <50876799+MBM9@users.noreply.github.com>
2019-11-13 12:09:18 +0200

    Delete main.cpp

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello Easy C++ project!" << std::endl;
-	std::cout << "hi\n";
-	std::cout << "yakir" << std::endl;
-}/* condition */
