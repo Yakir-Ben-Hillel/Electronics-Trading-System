566bcc75d635a1561bf09a6a8abad6df7c877b80
Yakir Ben Hillel <yakir985@gmail.com>
2019-11-13 11:21:02 +0200

    L

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello Easy C++ project!" << std::endl;
+	std::cout << "hi\n";
+	std::cout << "yakir" << std::endl;
+}/* condition */
