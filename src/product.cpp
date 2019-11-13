d17fdb60d89e7ae7b67bda0fa13cecfe8e5ec7cb
Maor <50876799+MBM9@users.noreply.github.com>
2019-11-13 14:29:55 +0200

    Update product.cpp

--- src/product.cpp
+++ src/product.cpp
@@ -1,8 +1,41 @@
 #include "product.h"
 
-Product:: bool setName(const char* name)
+bool Product::setName(const char* name)
 {
 
 }
 
-Product::
+bool Product::setPrice(float price)
+{
+
+}
+
+bool Product:: setSerialNumber(unsigned int serial)
+{
+
+}
+
+bool Product:: setCategory(eCategory category)
+{
+
+}
+
+const char* Product::getName() const
+{
+
+}
+
+float Product:: getPrice() const
+{
+
+}
+
+Product:: eCategory Product:: getCategory() const
+{
+
+}
+
+unsigned int Product:: getSerialNumber() const
+{
+    
+}
