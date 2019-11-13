f0c9429403f9097e59bff8f16b10c924a3518d19
Maor <50876799+MBM9@users.noreply.github.com>
2019-11-13 12:15:50 +0200

    Create seller.h

---
+++ include/seller.h
@@ -0,0 +1,23 @@
+#ifndef __Seller_H
+#define __Seller_H
+#include "system.h"
+#include "product.h"
+
+
+
+class Seller
+{
+    public:
+	   enum efeedback { A = 'A', B, C, D, E };//A is like 5 stars and E is like 1 star
+
+
+
+    private:
+		efeedback curr_fidback;
+		char* s_user_name;
+		char* s_address;
+		char s_password[10];//max length of password is 10 chars,can be letter or a number
+		Product** s_stock;
+};
+
+#endif // !__Seller_H
