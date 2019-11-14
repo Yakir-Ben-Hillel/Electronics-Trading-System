91a3e134570c0bf08eb448d244470a826dd69388
MBM9 <maor9920@gmail.com>
2019-11-14 13:20:45 +0200

    compiler

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 //electorincs project by Maor Ben Moshe,ID:316041466 and Yakir Ben Hillel,ID:209509348
-#include "system.h"
+#include "./include/system.h"
 
-
-void main()
+int main()
 {
-    
+   
 }
