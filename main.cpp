5ba5a1b92e99836a5fd09c3f176638bbb21396f0
MBM9 <maor9920@gmail.com>
2019-11-14 13:44:34 +0200

    hhh

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 //electorincs project by Maor Ben Moshe,ID:316041466 and Yakir Ben Hillel,ID:209509348
 #include "./include/system.h"
+using namespace std;
 
 int main()
 {
-   
+   cout<<"hello"<<endl;
+   system("pause");
 }
