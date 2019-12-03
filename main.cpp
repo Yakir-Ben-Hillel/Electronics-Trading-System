f818493cc52079001c5e1c7b496d3fe1bf8b3fd0
MBM9 <maor9920@gmail.com>
2019-12-03 19:47:38 +0200

    bla

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-//electorincs project by Maor Ben Moshe,ID:316041466 and Yakir Ben Hillel,ID:209509348
+/*electorincs project by Maor Ben Moshe,ID:316041466 and Yakir Ben Hillel,ID:209509348
+we are both on the class of Keren ,day:Sunday*/
 #include "./include/system.h"
 int main()
 {
